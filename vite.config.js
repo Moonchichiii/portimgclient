99dbd53eb2841fac5945bc39cef5e45f402b2e2b
Mats Gustafsson <113367401+Moonchichiii@users.noreply.github.com>
2024-06-26 23:12:24 +0200

    chore: Update vite.config.js build configuration

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/static/',
   build: {
     outDir: 'dist',
+    manifest: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/main.jsx'),
+      },
+    },
   },
 });
