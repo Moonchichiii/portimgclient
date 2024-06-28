fe951f8955159f4d9c573fd7e94413b8ae916a6e
Mats Gustafsson <113367401+Moonchichiii@users.noreply.github.com>
2024-06-28 12:48:58 +0200

    Remove unnecessary frontend assets and update page components

--- dist/assets/About-h6_n4bGB.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./main-Bn22BZz-.js";const o=()=>e.jsxs("div",{children:[e.jsx("h1",{children:"About Me"}),e.jsx("p",{children:"This is the about page of the portfolio website."})]});export{o as default};
