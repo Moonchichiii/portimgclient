4483caa35531ddd875d44a8d72b7501fc45f816d
Mats Gustafsson <113367401+Moonchichiii@users.noreply.github.com>
2024-06-28 16:10:13 +0200

    Remove unnecessary frontend assets and update page components

--- dist/assets/Portfolio-BaJ_kcec.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./main-BiQHBlKm.js";const t=()=>o.jsxs("div",{children:[o.jsx("h1",{children:"Portfolio"}),o.jsx("p",{children:"This is the portfolio page of the website."})]});export{t as default};
