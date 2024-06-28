0e1f6d7d64d6420d427c0f22bcb7d42282b09358
Mats Gustafsson <113367401+Moonchichiii@users.noreply.github.com>
2024-06-28 16:10:00 +0200

    chore: Update background color in globals.css

---
+++ dist/assets/Dashboard-Du723NT-.js
@@ -0,0 +1,1 @@
+import{a,j as s}from"./main-BiQHBlKm.js";const t=()=>{const{user:e}=a(r=>r.auth);return s.jsxs("div",{children:[s.jsx("h2",{children:"Dashboard"}),s.jsxs("p",{children:["Welcome, ",e.username,"!"]})]})};export{t as default};
